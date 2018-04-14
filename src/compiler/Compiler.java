0f9242e991c73b75b24926126e0f8d29d854559f
Mohamed Hussein <mohamed.hussein.1.9.9.71.9.9.8@gmail.com>
2018-04-14 02:05:53 +0200

    temp gui

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -1,6 +1,8 @@
 package compiler;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Compiler {
@@ -8,15 +10,29 @@ public class Compiler {
     public static void main(String[] args) {
         
             JFrame f = new JFrame();
+            JButton exit = new JButton("Exit");
+            JButton start = new JButton("Start");
+            Container c = f.getContentPane();
             f.setExtendedState(JFrame.MAXIMIZED_BOTH);
             f.setUndecorated(true);
             f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             f.setLayout(null);
-            Container c = f.getContentPane();
             f.setBounds(0, 0, screenSize.width,screenSize.height);
             JTextArea code = new JTextArea();
             c.add(code);
-            code.setBounds(10*Dim.ratio_W,10*Dim.ratio_H,100*Dim.ratio_W,100*Dim.ratio_H);
+            c.add(exit);
+            c.add(start);
+            start.setBounds(20*Dim.ratio_W, 720*Dim.ratio_H, 100*Dim.ratio_W,40*Dim.ratio_H);
+            exit.setBounds(1250*Dim.ratio_W, 720*Dim.ratio_H, 100*Dim.ratio_W,40*Dim.ratio_H);
+            exit.addActionListener(new ActionListener(){
+                @Override
+                public void actionPerformed(ActionEvent ae) {
+                    System.exit(0);
+                }
+                
+            });
+            code.setBounds(10*Dim.ratio_W,10*Dim.ratio_H,1000*Dim.ratio_W,600*Dim.ratio_H);
+           
             f.setVisible(true);
     
     }
