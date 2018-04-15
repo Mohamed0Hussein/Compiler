113267568a9f31254de50a6774b16af62d3bca19
eslamtayseer <eslam_tayseer@yahoo.com>
2018-04-16 01:45:19 +0200

    Added input as a file

--- src/Scanner/MyScanner.java
+++ src/Scanner/MyScanner.java
@@ -1,28 +1,55 @@
 package Scanner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class MyScanner {
 
-    public static void main(String[] args) {
-        String S = "read x;\n" +
-"if 0 < x then {this is a comment}\n" +
-" fact := 1;\n" +
-" repeat\n" +
-" fact := fact * x;\n" +
-" x := x - 1\n" +
-" until x = 0;\n" +
-" write fact\n" +
-"end";
-        ArrayList <Token>tokens = Token.Tokenize(S);
-        for (int i =0; i<tokens.size();i++)
-{
-System.out.println( tokens.get(i));
-}
+public static void main(String[] args) {
+         try {
+            // FileReader file = new FileReader(MyScanner.class.getResource("Input.txt").toString());
+            // FileReader file = new FileReader(("D:\\GitHub\\Compiler\\src\\Scanner\\InputFile.txt"));
+           FileReader file = new FileReader(("InputFile.txt"));
+             BufferedReader input = new BufferedReader(file);
+          /*  String S = "read x;\n"
+                    + "if 0 < x then {this is a comment}\n"
+                    + " fact := 1;\n"
+                    + " repeat\n"
+                    + " fact := fact * x;\n"
+                    + " x := x - 1\n"
+                    + " until x = 0;\n"
+                    + " write fact\n"
+                    + "end";*/
+           String S = "";
+            
+           while(true){
+            
+            try{
+             S = S.concat(input.readLine());
+            }
+            catch (NullPointerException ex){
+                break;
+            }
+             
+         }
+             ArrayList<Token> tokens = Token.Tokenize(S);
+             for (int i = 0; i < tokens.size(); i++) {
+                 System.out.println(tokens.get(i));
+             }
 
-      
-    
+         } catch (FileNotFoundException ex) {
+             Logger.getLogger(MyScanner.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (IOException ex) {
+             Logger.getLogger(MyScanner.class.getName()).log(Level.SEVERE, null, ex);
+         }
+
     }
     
 }
